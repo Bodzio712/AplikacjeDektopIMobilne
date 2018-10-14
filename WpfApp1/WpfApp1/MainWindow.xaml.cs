b2ede3dabf9a6225b09fd0e702594ef2b88218f6
Murzyn <bodzio_chomik@wp.pl>
2018-10-14 18:34:18 +0200

    Koniec

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -24,5 +24,25 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var u = new Models.User(NameBox.Text, SurnameBox.Text, DateTime.Parse(Date.Text));
+                MessageBox.Show("Formularz został zapisany", "Sukces", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Wszystkie pola muszą być wypełnione", "Błąd danych", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            NameBox.Text = "";
+            SurnameBox.Text = "";
+            Date.Text = "";
+        }
     }
 }
---
+++ WpfApp1/WpfApp1/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Models
+{
+    class User
+    {
+        private String Name;
+        private String Surname;
+        private DateTime Birth;
+
+        public User(string name, string surname, DateTime birth)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Surname = surname ?? throw new ArgumentNullException(nameof(surname));
+            Birth = birth;
+        }
+    }
+}
